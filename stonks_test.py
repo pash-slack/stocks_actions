388e168bbec1e8be0e1edf6b96b876c22714ed61
pash-slack <pkaushik@slack-corp.com>
2020-09-17 13:38:44 -0700

    Update stonks_test.py

--- stonks_test.py
+++ stonks_test.py
@@ -10,11 +10,6 @@ class stonks_test(unittest.TestCase):
         response = sc.get_latest_info()
         self.assertTrue('zip' in response)
 
-
-    def test_intentionally_failing(self):
-        self.assertTrue(False)
-
-
 if __name__ == '__main__':
     print('Starting Kennel integration tests!')
     unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'), failfast=False)

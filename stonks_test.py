9721c51e1c7108aedd9e9b8d37ce3b061666515f
Pash Kaushik <pkaushik@slack-corp.com>
2020-09-17 13:35:22 -0700

    Add a failing test

--- stonks_test.py
+++ stonks_test.py
@@ -11,6 +11,10 @@ class stonks_test(unittest.TestCase):
         self.assertTrue('zip' in response)
 
 
+    def test_intentionally_failing(self):
+        self.assertTrue(False)
+
+
 if __name__ == '__main__':
     print('Starting Kennel integration tests!')
     unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'), failfast=False)
